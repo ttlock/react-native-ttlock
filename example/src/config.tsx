9dc696f56c73ede8b8e7b524d3be86d09160c16e
Sciener <513466979@qq.com>
2021-01-14 11:33:46 +0800

    Update config.tsx

--- example/src/config.tsx
+++ example/src/config.tsx
@@ -4,6 +4,8 @@ export default {
   wifi: undefined,
   wifiPassword: undefined,
   gatewayName: "My Gateway One",
+  //test account ttlock uid 17498,  https://api.ttlock.com/v3/user/getUid
   ttlockUid: 17498,
+   //test account ttlock login password (unencrypted password or md5 password)
   ttlockLoginPassword: "111111"
 }
