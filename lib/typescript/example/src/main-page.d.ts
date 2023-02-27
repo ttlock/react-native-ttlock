3276106a994e3ba4289f10565b1c2c15ee05620c
JLGB <chensg@sciener.cn>
2023-02-27 15:45:46 +0800

    update

--- lib/typescript/example/src/main-page.d.ts
+++
@@ -1,4 +0,0 @@
-declare const MainPage: ({ navigation }: {
-    navigation: any;
-}) => JSX.Element;
-export default MainPage;
--- lib/typescript/example/src/scan-lock-page.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (props: {
-    navigation: any;
-    route: any;
-}) => JSX.Element;
-export default _default;
