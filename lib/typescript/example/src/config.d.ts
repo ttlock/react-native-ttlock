879b3d3e60290ce8bf8b58bd2ecf6b5a6f380b7f
JLGB <chensg@sciener.cn>
2023-02-27 15:43:03 +0800

    new projection ios

--- lib/typescript/example/src/config.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    wifi: undefined;
-    wifiPassword: undefined;
-    gatewayName: string;
-    ttlockUid: number;
-    ttlockLoginPassword: string;
-};
-export default _default;
--- lib/typescript/example/src/gateway-page.d.ts
+++
@@ -1,5 +0,0 @@
-declare const GatewayPage: (props: {
-    navigation: any;
-    route: any;
-}) => JSX.Element;
-export default GatewayPage;
