f2f1b9a5895687becff810a8b0316acaad326a6b
xtc <tcymac@126.com>
2025-07-17 20:26:35 +0800

    android update lockError

--- android/src/main/java/com/reactnativettlock/model/TTLockErrorConverter.java
+++ android/src/main/java/com/reactnativettlock/model/TTLockErrorConverter.java
@@ -7,42 +7,113 @@ import com.ttlock.bl.sdk.entity.LockError;
  */
 public class TTLockErrorConverter {
 
-    public static final int reseted = 0;
-    public static final int fail = 18;
-    public static final int notSupportModifyPasscode = 31;
-    public static final int bluetoothOff = 32;
-    public static final int bluetoothConnectTimeount = 33;
-    public static final int bluetoothDisconnection = 34;
-    public static final int lockIsBusy = 35;
-    public static final int invalidLockData = 36;
-    public static final int invalidParameter = 37;
+  public static final int hadReseted = 0;
+  public static final int crcError = 1;
+  public static final int noPermisstion = 2;
+  public static final int wrongAdminCode = 3;
+  public static final int lackOfStorageSpace = 4;
+  public static final int inSettingMode = 5;
+  public static final int noAdmin = 6;
+  public static final int notInSettingMode = 7;
+  public static final int wrongDynamicCode = 8;
+  public static final int isNoPower = 9;
+  public static final int resetPasscode = 10;
+  public static final int updatePasscodeIndex = 11;
+  public static final int invalidLockFlagPos = 12;
+  public static final int ekeyExpired = 13;
+  public static final int passcodeLengthInvalid = 14;
+  public static final int samePasscodes = 15;
+  public static final int ekeyInactive = 16;
+  public static final int aesKey = 17;
+  public static final int fail = 18;
+  public static final int passcodeExist = 19;
+  public static final int passcodeNotExist = 20;
+  public static final int lackOfStorageSpaceWhenAddingPasscodes = 21;
+  public static final int invalidParaLength = 22;
+  public static final int cardNotExist = 23;
+  public static final int fingerprintDuplication = 24;
+  public static final int fingerprintNotExist = 25;
+  public static final int invalidCommand = 26;
+  public static final int inFreezeMode = 27;
+  public static final int invalidClientPara = 28;
+  public static final int lockIsLocked = 29;
+  public static final int recordNotExist = 30;
+  public static final int wrongSSID = 31;
+  public static final int wrongWifiPassword = 32;
+  public static final int bluetoothPoweredOff = 33;
+  public static final int connectionTimeout = 34;
+  public static final int disconnection = 35;
+  public static final int lockIsBusy = 36;
+  public static final int wrongLockData = 37;
+  public static final int invalidParameter = 38;
 
     public static int native2RN(LockError error) {
-        int errorCode = error.getIntErrorCode();
-        if (errorCode >= 1 && errorCode <= 0x08) {
-            return errorCode;
-        }
-
-        if (errorCode >= 0xa && errorCode <= 0x1f) {
-            return errorCode - 1;
-        }
-
-        if (errorCode >= 0x20) {//31~37 ios customized error code
-            switch (error) {
-                case Failed:
-                case AES_PARSE_ERROR:
-                    return fail;
-                case LOCK_CONNECT_FAIL:
-                    return bluetoothConnectTimeount;
-                case LOCK_IS_BUSY:
-                    return lockIsBusy;
-                case DATA_FORMAT_ERROR:
-                    return invalidParameter;
-                case KEY_INVALID:
-                    return reseted;
-            }
-        }
-
-        return fail;
+      switch (error) {
+        case KEY_INVALID:
+          return hadReseted;
+        case LOCK_CRC_CHECK_ERROR:
+          return crcError;
+        case LOCK_NO_PERMISSION:
+          return noPermisstion;
+        case LOCK_ADMIN_CHECK_ERROR:
+          return wrongAdminCode;
+        case LOCK_IS_IN_SETTING_MODE:
+          return inSettingMode;
+        case LOCK_NOT_EXIST_ADMIN:
+          return noAdmin;
+        case LOCK_IS_IN_NO_SETTING_MODE:
+          return notInSettingMode;
+        case LOCK_DYNAMIC_PWD_ERROR:
+          return wrongDynamicCode;
+        case LOCK_NO_POWER:
+          return isNoPower;
+        case LOCK_INIT_KEYBOARD_FAILED:
+          return resetPasscode;
+        case LOCK_KEY_FLAG_INVALID:
+          return invalidLockFlagPos;
+        case LOCK_USER_TIME_EXPIRED:
+          return ekeyExpired;
+        case LOCK_PASSWORD_LENGTH_INVALID:
+          return passcodeLengthInvalid;
+        case LOCK_SUPER_PASSWORD_IS_SAME_WITH_DELETE_PASSWORD:
+          return samePasscodes;
+        case LOCK_USER_TIME_INEFFECTIVE:
+          return ekeyInactive;
+        case LOCK_PASSWORD_EXIST:
+          return passcodeExist;
+        case LOCK_PASSWORD_NOT_EXIST:
+          return passcodeNotExist;
+        case LOCK_NO_FREE_MEMORY:
+          return lackOfStorageSpace;
+        case LOCK_REVERSE:
+          return lockIsLocked;
+        case INVALID_VENDOR:
+          return invalidClientPara;
+        case INVALID_COMMAND:
+          return invalidCommand;
+        case LOCK_FROZEN:
+          return inFreezeMode;
+        case BAD_WIFI_NAME:
+          return wrongSSID;
+        case BAD_WIFI_PASSWORD:
+          return wrongWifiPassword;
+        case IC_CARD_NOT_EXIST:
+          return cardNotExist;
+        case FINGER_PRINT_NOT_EXIST:
+          return fingerprintNotExist;
+        case INVALID_PARAM:
+          return invalidParameter;
+        case RECORD_NOT_EXIST:
+          return recordNotExist;
+        case Failed:
+          return fail;
+        case LOCK_CONNECT_FAIL:
+          return connectionTimeout;
+        case LOCK_IS_BUSY:
+          return lockIsBusy;
+        case DATA_FORMAT_ERROR:
+          return invalidParameter;
+      }
+      return fail;
     }
 }
