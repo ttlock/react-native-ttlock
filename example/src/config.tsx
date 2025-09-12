81f11f10c4d0d583f0c6ee678d729a785ac3b8a9
lujinbo <chensg@sciener.cn>
2025-09-12 09:38:33 +0800

    no message

--- example/src/config.tsx
+++ example/src/config.tsx
@@ -5,7 +5,7 @@ export default {
   wifiPassword: undefined,
   gatewayName: "My Gateway 1",
   //test account ttLock uid 17498,  https://api.ttlock.com/v3/user/getUid
-  ttLockUid: 17498,
+  ttLockUid: 56033,
    //test account ttLock login password (unencrypted password or md5 password)
-  ttLockLoginPassword: "111111"
+  ttLockLoginPassword: "96e79218965eb72c92a549dd5a330112"
 }
