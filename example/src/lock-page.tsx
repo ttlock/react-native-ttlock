9db2d84e6d60e44187ffe275c63ec6887d856ee5
lujinbo <chensg@sciener.cn>
2026-05-29 14:58:55 +0800

    修改readme

--- example/src/lock-page.tsx
+++ example/src/lock-page.tsx
@@ -580,7 +580,7 @@ const operationClick = (lockOperation: string, lockData: string, lockMac: string
     }, failedCallback)
   }
   else if (lockOperation === "Wifi lock config server") {
-    Ttlock.configServer("121.196.45.100", "4999", lockData, () => {
+    Ttlock.configServer("121.196.45.xxxx", "xx99", lockData, () => {
       let text = "config lock wifi ip address success";
       successCallback(text);
     }, failedCallback)
